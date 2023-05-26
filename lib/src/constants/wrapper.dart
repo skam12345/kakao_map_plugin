a3b4bf4c4c5cd673fbc7330185483f8ef83d8eea
skam1234561 <skam1234561@gmail.com>
2023-05-26 16:52:55 +0900

    커밋

--- lib/src/constants/wrapper.dart
+++ lib/src/constants/wrapper.dart
@@ -10,39 +10,16 @@ String _htmlWrapper(String script) {
   <script type="text/javascript"
           src='https://dapi.kakao.com/v2/maps/sdk.js?autoload=true&appkey=${AuthRepository.instance.appKey}&libraries=services,clusterer,drawing'></script>
   <style>
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
-    * {
-        font-family: 'Nanum Gothic Coding', monospace;
-    }
-    .custom-overlay {
-        position: absolute;
-        width: 300px;
-        height: 200px;
-        border-radius: 14px;
-        display: flex;
-        flex-direction: column;
-        background-color: #EFEFEF;
-    }
-    
-    .custom-overlay::after {
-      border-top: 20px solid #EFEFEF;
-      border-left: 20px solid transparent;
-      border-right: 20px solid transparent;
-      border-bottom: 0px solid transparent;
-      content: "";
-      position: absolute;
-      top: 200px;
-      left: 135px;
-    }
-
-    ol {
-        list-style: none;
-    }
+      @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
+      * {
+          font-family: 'Nanum Gothic Coding', monospace;
+      }
   </style>
 </head>
 
 <body style="margin: 0;">
 <div id="map" style="width: 100vw; height: 100vh;"></div>
+
 
 $script
 
