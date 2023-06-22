299a1be084f42e18e1165d4cb25c3d04248f227e
skam1234561 <skam1234561@gmail.com>
2023-06-22 09:26:32 +0900

    테스트 02

--- lib/src/basic/kakao_map.dart
+++ lib/src/basic/kakao_map.dart
@@ -624,7 +624,7 @@ class _KakaoMapState extends State<KakaoMap> {
     customOverlays.push(customOverlay);
   }
 
-  function customTap(id, latitude, longitude) {
+  function customTap(id) {
     // var lat = parseFloat(latitude);
     // var lng = parseFloat(longitude);
 
